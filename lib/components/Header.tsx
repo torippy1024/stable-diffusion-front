be561ef83c3da5e29ec73bba5a6bda3108e276d4
h.toriyama <toriyamahiroto1128@gmail.com>
2022-12-17 11:26:21 +0900

    自分用テンプレートページ追加

---
+++ lib/components/Header.tsx
@@ -0,0 +1,48 @@
+import { AiOutlineDown, AiOutlineMenu } from "react-icons/ai";
+
+type MenuType = {
+  horizontal: boolean;
+};
+
+const Header = () => {
+  const Menu = ({horizontal}: MenuType) => 
+    <ul className={`menu ${horizontal ? "menu-horizontal" : "bg-base-200"} p-0`}>
+      <li><a>hoge1</a></li>
+      <li><a>hoge2</a></li>
+      <li tabIndex={0}>
+        <a>
+          Parent
+          <AiOutlineDown />
+        </a>
+        <ul className="p-2 bg-base-200">
+          <li><a>Submenu 1</a></li>
+          <li><a>Submenu 2</a></li>
+        </ul>
+      </li>
+    </ul>;
+
+  return (
+    <div className="navbar bg-base-200">
+      <div className="md:hidden flex-none">
+          <ul className="menu menu-horizontal p-0 bg-base-200">
+            <li tabIndex={0} className="z-10">
+              <a>
+                <AiOutlineMenu />
+              </a>
+              <Menu horizontal={false} />
+            </li>
+          </ul>
+      </div>
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">Next.js</a>
+      </div>
+      <div className="hidden md:block flex-none">
+        <ul className="menu menu-horizontal p-0">
+          <Menu horizontal={true} />
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
