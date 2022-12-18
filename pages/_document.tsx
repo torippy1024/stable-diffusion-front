02c771533a0f2c043693650fca8fd285a7687851
h.toriyama <toriyamahiroto1128@gmail.com>
2022-12-18 14:56:18 +0900

    metaタグ設定

---
+++ pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, {Html, Head, Main, NextScript} from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='ja'>
+        <Head>
+          <meta charSet='utf-8' />
+          <meta name='format-detection' content='telephone=no' />
+
+          <meta property='og:site_name' content='サイト名' />
+          <meta property='og:title' content='タイトル' />
+          <meta property='og:description' content='説明文' />
+          <meta property='og:type' content='website' />
+          <meta property='og:url' content='URL' />
+          <meta property='og:image' content='https://shima-usa.net/web-template/favicon.ico' />
+          <meta name='twitter:card' content='summary' />
+          <meta name='twitter:site' content='URL' />
+          <meta name='twitter:image' content='https://shima-usa.net/web-template/favicon.ico' />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
