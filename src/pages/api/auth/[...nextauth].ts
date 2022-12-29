b16a0e209122ca808b4e5b3eb95d78a0e05307b5
h.toriyama <toriyamahiroto1128@gmail.com>
2022-12-29 20:29:29 +0900

    next-authを導入

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,95 @@
+import NextAuth, {NextAuthOptions} from 'next-auth';
+import SpotifyProvider from 'next-auth/providers/spotify';
+
+const scope = [
+  'playlist-read-private',
+  'streaming',
+  'user-read-private',
+  'user-library-read',
+  'user-read-playback-state',
+  'user-modify-playback-state',
+  'user-read-recently-played',
+  'user-follow-read',
+];
+
+type RefreshResponseType = {
+  access_token: string;
+  refresh_token?: string;
+  token_type: string;
+  expires_in: number;
+  scope: string;
+};
+
+const refreshAccessToken = async (token: any) => {
+  try {
+    const url =
+      'https://accounts.spotify.com/api/token?' +
+      new URLSearchParams({
+        client_id: process.env.SPOTIFY_CLIENT_ID || '',
+        client_secret: process.env.SPOTIFY_CLIENT_SECRET || '',
+        grant_type: 'refresh_token',
+        refresh_token: token.refreshToken,
+      });
+
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      method: 'POST',
+    });
+
+    const refreshedTokens = (await response.json()) as RefreshResponseType;
+
+    if (!response.ok) {
+      throw refreshedTokens;
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError',
+    };
+  }
+};
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID || '',
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET || '',
+      authorization: `https://accounts.spotify.com/authorize?scope=${scope.join()}`,
+    }),
+  ],
+  secret: process.env.SPOTIFY_CLIENT_SECRET || '',
+  callbacks: {
+    async jwt({token, account}) {
+      if (account) {
+        token.id = account.id;
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.accessTokenExpires =
+          account.expires_at && account.expires_at * 1000;
+      }
+      if (token.accessTokenExpires && Date.now() < token.accessTokenExpires) {
+        return token;
+      }
+
+      return refreshAccessToken(token);
+    },
+    async session({session, token}) {
+      // session.token = token;
+
+      return session;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,40 @@
 import type {NextPage} from 'next';
+import {signIn, signOut, useSession} from 'next-auth/react';
 import Link from 'next/link';
 import Layout from '../components/layouts/Layout';
 
 const Home: NextPage = () => {
+  const {data} = useSession();
   return (
     <Layout>
       <div>hogehoge</div>
       <div className='btn btn-primary'>
         <Link href='/page2'>link to page2</Link>
       </div>
+      {data && (
+        <button
+          key='signOut'
+          className='btn mr-2'
+          onClick={(e) => {
+            e.preventDefault();
+            signOut();
+          }}
+        >
+          sign out
+        </button>
+      )}
+      {!data && (
+        <button
+          key='signIn'
+          className='btn mr-2'
+          onClick={(e) => {
+            e.preventDefault();
+            signIn('spotify');
+          }}
+        >
+          sign in
+        </button>
+      )}
     </Layout>
   );
 };
