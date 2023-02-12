6c5d5288f640b19c9a3b3718838aca0b2902fecb
torippy <47879788+torippy1024@users.noreply.github.com>
2023-02-12 14:51:45 +0900

    commonPrompt追加

---
+++ src/const/diffusion.ts
@@ -0,0 +1,7 @@
+const DIFFUSION_CONFIG = {
+  COMMON_PROMPT: 'masterpiece, best quality, high quality, absurdres',
+  COMMON_NEGATIVE_PROMPT:
+    '{{{{{bad anatomy}}}}}, {{{{bad hands}}}}, {{{worst quality}}}, {{low quality}}, {normal quality}',
+};
+
+export default DIFFUSION_CONFIG;
